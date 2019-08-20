e3c1e246d96c607ca3438ef3701a26bc5e802307
RsRuman <rsbdruman@gmail.com>
2019-08-20 21:41:40 +0600

    Working on Create,Update,Delete

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,20 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Middleware\RedirectIfAuthenticated;
 use App\Model\Admin;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Resources\User\UserResource;
 use App\Http\Resources\User\UserCollection;
 use PhpParser\Node\Stmt\Echo_;
 
 class AdminController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    //construct is define for POST,PUT
+
+//    public function __construct()
+//    {
+//        $this->middleware('auth:api')->except('index', 'show');
+//    }
+
     public function index()
     {
         $userList = UserCollection::collection(User::all());
@@ -23,36 +27,37 @@ class AdminController extends Controller
         return view('adminPanel', ['userList'=> $userList]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'phone' => 'required',
+            'city' => 'required',
+            'password' => 'required'
+        ]);
+
+
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->phone = $request->phone;
+        $user->city = $request->city;
+        $user->password = Hash::make($request->password);
+
+        $user->save();
+        return redirect()->route('admins.index');
+
+
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Model\Admin  $admin
-     * @return \Illuminate\Http\Response
-     */
     public function show(User $admin)
     {
-        return new UserResource($admin);
+        $userList = UserCollection::collection(User::all());
+        $userUpdate = $admin;
+        return view('adminPanel',['userUpdate' => $userUpdate, 'userList'=> $userList]);
     }
 
     /**
@@ -75,7 +80,9 @@ class AdminController extends Controller
      */
     public function update(Request $request, User $admin)
     {
-        echo "Update";
+        $admin->update($request->all());
+        return redirect('admins.index');
+
     }
 
     /**
@@ -86,6 +93,7 @@ class AdminController extends Controller
      */
     public function destroy(User $admin)
     {
-        echo "Destroy";
+        $admin->delete();
+        return redirect('admins.index');
     }
 }
--- resources/views/adminPanel.blade.php
+++ resources/views/adminPanel.blade.php
@@ -10,6 +10,50 @@
                     <div class="card-body">
                         <h1>Simple Admin Panel</h1>
 
+                        @if(isset($userUpdate))
+                            <form method="put" action="{{route('admins.update',$userUpdate->id)}}">
+                                @method('PUT')
+                                @csrf
+                                <div class="form-group">
+                                    <input type="text" name="name" value="{{$userUpdate->name}}" class="form-control" placeholder="Enter Name" required autocomplete="name" autofocus>
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" name="email" value="{{$userUpdate->email}}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required autocomplete="email" autofocus>
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" name="phone" value="{{$userUpdate->phone}}" class="form-control"   placeholder="Enter Phone" required autocomplete="phone" autofocus>
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" name="city" value="{{$userUpdate->city}}" class="form-control"   placeholder="Enter city" required autocomplete="city" autofocus>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </form>
+
+                        @else
+
+                        <!-- Form start here -->
+                        <form method="post" action="{{route('admins.store')}}">
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <input type="text" name="name" value="" class="form-control" placeholder="Enter Name" required autocomplete="name" autofocus>
+                            </div>
+                            <div class="form-group">
+                                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required autocomplete="email" autofocus>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" name="phone" class="form-control"   placeholder="Enter Phone" required autocomplete="phone" autofocus>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" name="city" class="form-control"   placeholder="Enter city" required autocomplete="city" autofocus>
+                            </div>
+                            <div class="form-group">
+                                <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required autocomplete="password" autofocus>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                        @endif
+                        <br>
+
                         <!-- Bootstrap table -->
                         <table class="table table-dark">
                             <thead>
@@ -31,13 +75,14 @@
                                     <td>{{$user->phone}}</td>
                                     <td>{{$user->city}}</td>
                                     <td>
-                                        <a href='{{route('admins.update', $user->id)}}'>
+                                        <a href='{{route('admins.show', $user->id)}}'>
                                         <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
                                         </a>
-                                        <a href='{{route('admins.destroy', $user->id)}}'>
-                                        <i class="fa fa-trash" aria-hidden="true"></i>
-                                        </a>
                                     </td>
+                                    <td><a href='{{route('admins.destroy', $user->id)}}'>
+                                            <i class="fa fa-trash" aria-hidden="true"></i>
+                                        </a></td>
+
                                 </tr>
                             @endforeach
                             </tbody>
